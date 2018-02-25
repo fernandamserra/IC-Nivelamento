23d8f5a6d69003d4a882ff3869b37c3921925ece
Fernanda Serra <glassh3arts@gmail.com>
2018-02-25 01:31:47 -0300

    Jogo da forca finalizado.

--- forca.py
+++ forca.py
@@ -6,14 +6,15 @@ for i in range(10) :
 
 print("********* INICIO **********")
 
+suicida = ["||=====:====","||     :     ","||","||","||","||","================","||     O","||     |","||    /|","||    /|\ ","||    / ","||    / \    *****GAMEOVER*****"]
 letras = []
 chutes = [""]
 erros = 0
+gameover = False
 
 for i in range(len(palavra)) :
     letras.append(" _ ")
 
-gameover = False
 while gameover == False :
     print("\n")
 
@@ -34,11 +35,57 @@ while gameover == False :
     if chute not in palavra :
         print("\n")
         erros += 1
-        print("Você agora possui ", erros ,"/6 erros.")
 
-    if erros == 6 :
+    print("\n")
+    if erros == 0 :
+        for i in range(0, 7, 1):
+            print(suicida[i])
+    elif erros == 1 :
+        for i in range(0, 2, 1):
+            print(suicida[i])
+        print(suicida[7])
+        for i in range(3, 7, 1):
+            print(suicida[i])
+    elif erros == 2 :
+        for i in range(0, 2, 1):
+            print(suicida[i])
+        print(suicida[7])
+        print(suicida[8])
+        for i in range(4, 7, 1):
+            print(suicida[i])
+    elif erros == 3 :
+        for i in range(0, 2, 1):
+            print(suicida[i])
+        print(suicida[7])
+        print(suicida[9])
+        for i in range(4, 7, 1):
+            print(suicida[i])
+    elif erros == 4 :
+        for i in range(0, 2, 1):
+            print(suicida[i])
+        print(suicida[7])
+        print(suicida[10])
+        for i in range(4, 7, 1):
+            print(suicida[i])
+    elif erros == 5 :
+        for i in range(0, 2, 1):
+            print(suicida[i])
+        print(suicida[7])
+        print(suicida[10])
+        print(suicida[11])
+        for i in range(5, 7, 1):
+            print(suicida[i])
+    else :
         gameover = True
 
+        for i in range(0, 2, 1):
+            print(suicida[i])
+        print(suicida[7])
+        print(suicida[10])
+        print(suicida[12])
+        for i in range(5, 7, 1):
+            print(suicida[i])
+
 else :
-    print("this shit is over")
+    print("\n")
     print("A palavra secreta era '", palavra ,"'.")
