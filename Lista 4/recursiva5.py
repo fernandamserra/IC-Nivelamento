4b10f1105b689aff3976ef3ce6fd681f47bc935c
Fernanda Serra <glassh3arts@gmail.com>
2018-03-03 13:08:18 -0300

    Apaguei umas questões com bug

--- Lista 4/recursiva5.py
+++
@@ -1,8 +0,0 @@
-def contagem(num) :
-    for i in range(num + 1) :
-        print(i, end =" ")
-
-    print("\n")
-
-x = int(input("Digite um número inteiro :"))
-contagem(x)
--- Lista 4/recursive.py
+++
@@ -1,8 +0,0 @@
-def soma(lista) :
-    if len(lista) == 0 :
-        return 0;
-    else:
-        head, *tail = lista
-        return head + soma(tail)
-
-soma([1,2,3])
